006d307560049e48d6e51ccd9be4d19f20ac340c
Ansari Majid Ali <86536394+Ansarimajid@users.noreply.github.com>
2022-12-09 20:46:06 +0530

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from .views import *
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = ...
+
+urlpatterns += staticfiles_urlpatterns()
+
 urlpatterns = [
     path('', index, name='home'),
     path('create_user', create_user, name = 'create_user'),
